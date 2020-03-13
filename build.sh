7e13649ff6fade80c8b200e9c48f84f902ab3c85
Johnathan Van Why <jrvanwhy@google.com>
2020-03-13 15:30:09 -0700

    Fix the build: we need to specify RUSTFLAGS.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo build --release --target=thumbv7em-none-eabi --features=with_ctap1
+RUSTFLAGS="-C link-arg=-Tnrf52840dk_layout.ld -C relocation-model=static" cargo build --release --target=thumbv7em-none-eabi --features=with_ctap1
